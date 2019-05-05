8046acfc64257dd115c39d7c2fde8c48190b7f28
minhduc <minhduc1994vcu@gmail.com>
2019-05-05 18:02:19 +0700

    create Fan class

---
+++ src/main/java/com/codegym/Fan.java
@@ -0,0 +1,71 @@
+package com.codegym;
+
+public class Fan {
+    private final int slow = 1, medium = 2, fast = 3;
+    private int speed = slow;
+    private boolean on = false;
+    private double radius = 5;
+    private String color = "blue";
+
+    public int getSlow() {
+        return slow;
+    }
+
+    public int getMedium() {
+        return medium;
+    }
+
+    public int getFast() {
+        return fast;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Fan() {
+    }
+
+    @Override
+    public String toString() {
+        if (on) {
+            return "Fan is on " +
+                    "speed=" + speed +
+                    ", radius=" + radius +
+                    ", color=" + color;
+        } else {
+            return "Fan is off" +
+                    ", radius=" + radius +
+                    ", color=" + color;
+        }
+
+    }
+}
